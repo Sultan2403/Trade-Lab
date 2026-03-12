1645e8d9ee15c6565eded0f62323ba9257f92f7e
Sultan2403 <sultanremilekun@gmail.com>
2026-03-12 02:50:13 +0100

    removed deprecated stuff from ui input component

--- Frontend/src/Components/UI/Common/input.jsx
+++ Frontend/src/Components/UI/Common/input.jsx
@@ -1,32 +1,39 @@
-import { TextField } from "@mui/material";
+import { TextField, InputAdornment } from "@mui/material";
 
-export default function UIInput({ error, helperText, sx, ...props }) {
+export default function UIInput({
+  startIcon,
+  endIcon,
+  error,
+  helperText,
+  sx,
+  ...rest
+}) {
   return (
     <TextField
+      {...rest}
       variant="outlined"
       size="small"
       fullWidth
       error={Boolean(error)}
       helperText={helperText || ""}
+      slotProps={{
+        input: {
+          startAdornment: startIcon ? (
+            <InputAdornment position="start">{startIcon}</InputAdornment>
+          ) : undefined,
+          endAdornment: endIcon ? (
+            <InputAdornment position="end">{endIcon}</InputAdornment>
+          ) : undefined,
+        },
+      }}
       sx={{
         "& .MuiOutlinedInput-root": {
           borderRadius: "12px",
           fontSize: "0.875rem",
           backgroundColor: "#fff",
         },
-
-        "& .MuiInputBase-input": {
-          padding: "10px 12px",
-        },
-
-        "& .MuiFormHelperText-root": {
-          marginLeft: "2px",
-          fontSize: "0.75rem",
-        },
-
         ...sx,
       }}
-      {...props}
     />
   );
 }
