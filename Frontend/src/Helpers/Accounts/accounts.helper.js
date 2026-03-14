8d2f39c71ff5a46cebcba72fedbcf922c729e713
Sultan2403 <sultanremilekun@gmail.com>
2026-03-14 19:54:13 +0100

    frontend reset

--- Frontend/src/Helpers/Accounts/accounts.helper.js
+++
@@ -1,9 +0,0 @@
-const ACCOUNT_ID_KEY = "account_id";
-
-export function getAccountId() {
-  return localStorage.getItem(ACCOUNT_ID_KEY);
-}
-
-export function setAccountId(newId) {
-  localStorage.setItem(ACCOUNT_ID_KEY, newId);
-}
